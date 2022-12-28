5ed1348a1304b7fa2bbd5d0bbca2d2eff4919dbf
Ekaterina <katt_217@mail.ru>
2022-12-28 16:36:09 +0800

    решение задачи

---
+++ Finwork1/Program.cs
@@ -0,0 +1,35 @@
+﻿string[] FillArray()
+{
+    string[] array = new string[] {"Hello", "2", "world", ":-)"};
+    return array;
+}
+
+void PrintArray (string[] array)
+{
+    for (int i=0; i < array.Length; i++)
+    {       
+        if (array[i] != String.Empty) Console.Write("'" + array[i] + "'");
+    }
+    Console.WriteLine();
+}
+
+string[] NewStringArray (string[] array)
+{
+    int j = 0;
+    int count = 3;
+    string[] newArray = new string[array.Length];
+    for (int i=0; i < array.Length; i++)
+    {
+        if (array[i].Length < count + 1)
+        {
+           newArray[j] = array[i];
+           j++;
+        }
+    }
+return newArray;
+}
+
+string[] array = FillArray();
+PrintArray(array);
+string[] newArray = NewStringArray(array);
+PrintArray(newArray);
